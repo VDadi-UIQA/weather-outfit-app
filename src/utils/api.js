b24d3c6cd7794b778f35250d304f4d9e6e88c22b
VDadi-UIQA <vasanthabharath1986@gmail.com>
2025-07-24 18:52:58 +0530

    apikey chanaged

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,9 +7,12 @@ const Weather = () => {
 
   useEffect(() => {
     const fetchWeather = async () => {
+     //const API_KEY = 'b6b1aec3ede651a2f12dd560dee82d35';
      const API_KEY = 'b6b1aec3ede651a2f12dd560dee82d35'; 
+     
       const city = 'Hyderabad';
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+      //const url = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b6b1aec3ede651a2f12dd560dee82d35`;
 
       try {
         const res = await fetch(url);
